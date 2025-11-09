f63231ad5928974d2ac3ebf62c6629e87452497f
Kevin A Hernandez Garcia <kevinalbertohernandezgarcia@gmail.com>
2025-11-09 03:06:05 -0600

    added the most atrociud form know to man

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+import About from './About'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>  
+        <Route path='/' element={<App />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
